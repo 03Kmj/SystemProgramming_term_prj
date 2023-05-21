53c05687adb3551d0c4f892936641c2bab13b0ce
seokhyunpark <122147233+seokhyunpark@users.noreply.github.com>
2023-05-21 15:16:45 +0900

    Update main.c

--- main.c
+++ main.c
@@ -1,8 +1,6 @@
 #include <stdio.h>
-
-void similar() {
-    printf("similar check\n");
-}
+#include <stdlib.h>
+#include <unistd.h>
 
 int main(int argc, char *argv[]) {
     if (argc < 3) {
@@ -10,11 +8,23 @@ int main(int argc, char *argv[]) {
     }
     char *main_dir = argv[1];
     printf("main_dir: %s\n", main_dir);
+    sleep(1);
 
     for (int i = 2; i < argc; i++) {
         char *dir = argv[i];
-        printf("dir%d: %s\n",i - 1, dir);
-        similar();
+        char command[100];
+        sprintf(command, "python3 ./similarity.py %s %s", main_dir, dir);
+        system(command);
+
+        FILE *fp = fopen("similarity.txt", "r");
+        if (fp == NULL) {
+            printf("Error: cannot open file\n");
+            return 1;
+        }
+        double similarity;
+        fscanf(fp, "%lf", &similarity);
+        printf("The similarity between %s and %s is %lf\n", main_dir, dir, similarity);
+        sleep(1);
     }
 
     return 0;
