863272face21cb1dabcd43f4bc9d0c954eb30444
seokhyunpark <122147233+seokhyunpark@users.noreply.github.com>
2023-05-21 14:23:22 +0900

    Update similarity.py
    - 파이썬 파일을 실행할 때 파일명을 넘거줘서 파일을 읽는 기능 추가
    - 유사도 검사 결과를 similarity.txt파일로 저장

--- similarity.py
+++ similarity.py
@@ -1,7 +1,11 @@
 import difflib
+import sys
 
-main_contents = 'printf("Hello World!\n");'
-sub_contents = 'fprintf("hello world?\n");)'
+main_file = sys.argv[1]
+sub_file = sys.argv[2]
+
+main_contents = open(main_file, 'r').read()
+sub_contents = open(sub_file, 'r').read()
 
 main_bytes = bytes(main_contents, 'utf-8')
 sub_bytes = bytes(sub_contents, 'utf-8')
@@ -11,4 +15,5 @@ sub_bytes_list = list(sub_bytes)
 sm = difflib.SequenceMatcher(None, main_bytes_list, sub_bytes_list)
 similar = sm.ratio() * 100
 
-print(round(similar, 2), "%")
+with open('similarity.txt', 'w') as f:
+    f.write((round(similar, 2)))
