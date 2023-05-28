4e5b97f3e251d072be17f702c22873a65ceb634d
강민정 <106760660+03Kmj@users.noreply.github.com>
2023-05-28 20:39:25 +0900

    Update progressbar.c
    속도 향상, 완전 출력

--- progressbar.c
+++ progressbar.c
@@ -1,34 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h> 
-#include <CoreFoundation/CoreFoundation.h> //macOS
-//windows: #include <windows.h>
+#include <unistd.h>
 
-int main(){
+int main() {
     const char bar = '=';
     const char blank = ' ';
-    const int LEN = 20;
-    const int MAX = 1000;
+    const int LEN = 50;
+    const int MAX = 100;
     const int SPEED = 35;
     int count = 0;
     int i;
-    float tick = (float)100/LEN;
-    printf("%.2f%% 마다 bar 1개 출력 \n\n", tick);
+    float tick = (float) MAX / LEN;
+    printf("%.2f%% 단위로 bar 1개 출력\n\n", tick);
     int barCount;
     float percent;
-    while(count < MAX){
-        printf("\r%d%d [", count, MAX);
-        percent = (float)count/MAX*100;
-        barCount = (int)(percent/tick);
-        for(i=0; i<LEN; i++){
-            if(i<barCount) printf("%c", bar);
-            else printf("%c", blank);
+    
+    while (count <= MAX) {
+        printf("\r[");
+        percent = (float) count / MAX * 100;
+        barCount = (int) (percent / tick);
+        for (i = 0; i < LEN; i++) {
+            if (i < barCount)
+                printf("%c", bar);
+            else
+                printf("%c", blank);
         }
         printf("] %.2f%%", percent);
+        fflush(stdout); // 출력 버퍼 비우기
         count++;
         usleep(SPEED * 1000);
     }
-    printf("done!\n\n");
+    printf("\ndone!\n\n");
     system("pause");
     return 0;
 }
