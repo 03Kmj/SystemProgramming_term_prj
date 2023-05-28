b5b8580484ab78d6e90e31237714df0d2f021896
seokhyunpark <122147233+seokhyunpark@users.noreply.github.com>
2023-05-28 18:05:18 +0900

    Update similarity.py
    파일 저장에서 print로 변경

--- similarity.py
+++ similarity.py
@@ -9,11 +9,11 @@ sub_contents = open(sub_file, 'r').read()
 
 main_bytes = bytes(main_contents, 'utf-8')
 sub_bytes = bytes(sub_contents, 'utf-8')
+
 main_bytes_list = list(main_bytes)
 sub_bytes_list = list(sub_bytes)
 
 sm = difflib.SequenceMatcher(None, main_bytes_list, sub_bytes_list)
 similar = sm.ratio() * 100
 
-with open('similarity.txt', 'w') as f:
-    f.write((round(similar, 2)))
+print(similar)
