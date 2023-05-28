9176fc1e7491ca0b45d9294b39396e8c2a10687a
seokhyunpark <122147233+seokhyunpark@users.noreply.github.com>
2023-05-28 18:06:44 +0900

    Create ls_c.c
    pipe를 사용하여 현재 디렉토리 내 c파일 리스트

---
+++ ls_c.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+int main() {
+    char *grep = "grep";
+    char *ls = "ls";
+    char *c = ".c";
+
+    int pid;
+    int thepipe[2];
+    if (pipe(thepipe) == -1) {
+        perror("pipe");
+        exit(1);
+    }
+    if ((pid = fork()) == -1) {
+        perror("fork");
+        exit(1);
+    }
+    if (pid > 0) {
+        close(thepipe[1]);
+        dup2(thepipe[0], 0);
+        close(thepipe[0]);
+        execlp(grep, grep, c, NULL);
+
+        perror("execlp");
+        exit(1);
+    }
+    close(thepipe[0]);
+    dup2(thepipe[1], 1);
+    close(thepipe[1]);
+    execlp(ls, ls, NULL);
+    
+    perror("execlp");
+    exit(1);
+}
