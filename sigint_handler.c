644a5205bd736caf16eb39276af3e7f27fdb370b
강민정 <106760660+03Kmj@users.noreply.github.com>
2023-05-29 23:07:28 +0900

    Update sigint_handler.c
    종료 여부를 묻는 시그널 -> 키보드 모든 입력 무시로 바꿈

--- sigint_handler.c
+++ sigint_handler.c
@@ -1,24 +1,27 @@
 #include <stdio.h>
 #include <signal.h>
 #include <stdlib.h>
-// SIGINT 핸들러 함수
-void sigint_handler(int signum) {
-    char choice;
-    printf("프로그램을 종료하시겠습니까? (y/n): ");
-    scanf(" %c", &choice);
-    if (choice == 'y' || choice == 'Y') {
-        exit(0);
-    }
+
+// 시그널 핸들러 함수
+void signal_handler(int signum) {
+    // 시그널 무시
 }
 
 int main() {
-    // SIGINT 핸들러 등록
-    signal(SIGINT, sigint_handler);
+    // 모든 시그널을 무시하는 핸들러 등록
+    struct sigaction sa;
+    sa.sa_handler = signal_handler;
+    sigemptyset(&sa.sa_mask);
+    sa.sa_flags = 0;
+    for (int i = 1; i <= 31; i++) {
+        sigaction(i, &sa, NULL);
+    }
 
     // 메인 프로그램 시작
     while (1) {
-       printf("working...\n");
-       sleep(1);
+        printf("working...\n");
+        sleep(1);
     }
+
     return 0;
 }
