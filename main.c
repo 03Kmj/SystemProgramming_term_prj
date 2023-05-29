70786569895afb7de4c60840f9afdc3a99d4947d
Widadada <106460325+Widadada@users.noreply.github.com>
2023-05-29 18:34:16 +0900

    Update main.c
    result 네모박스

--- main.c
+++ main.c
@@ -86,17 +86,46 @@ int main(int argc, char *argv[]) {
     fclose(fp);
     fclose(fp_result);
 
-    printf("\n\n-------------------------RESULT-------------------------\n");
+    printf("\n\n -------------------------RESULT-------------------------\n");
     fp_result = fopen("similarity_result.txt", "r");
     if (fp_result == NULL) {
         printf("Failed to open file: similarity_result.txt\n");
         exit(1);
     }
+
+    for (int i = 0; i < 2; i++) {
+        printf("|");
+        for (int j = 0; j < 55; j++) {
+            printf(" ");
+        }
+        printf("|\n");
+    }
+
     for (int i = 0; i < num_sub_files; i++) {
+        printf("| ");
         char buf[100];
         fgets(buf, sizeof(buf), fp_result);
+        int len = (55 - strlen(buf)) / 2;
+        for (int j = 0; j < len; j++) {
+            printf(" ");
+        }
         printf("%s", buf);
+
+        for (int j = 0; j < 55 - len - strlen(buf) - 1; j++) {
+            printf(" ");
+        }
+        printf("|\n");
     }
+
+    for (int i = 0; i < 2; i++) {
+        printf("|");
+        for (int j = 0; j < 55; j++) {
+            printf(" ");
+        }
+        printf("|\n");
+    }
+    
+    printf(" -------------------------------------------------------");
 
     return 0;
 }
