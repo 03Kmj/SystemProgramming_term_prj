078d85ed5fe0c73a2d1c78f1804482abc9486c82
Widadada <106460325+Widadada@users.noreply.github.com>
2023-05-29 19:37:36 +0900

    Update result.c

--- result.c
+++ result.c
@@ -1,4 +1,4 @@
-    printf("\n\n -----------------------------------------------RESULT----------------------------------------------\n");
+    printf("\n\n -----------------------------------RESULT----------------------------------\n");
     fp_result = fopen("similarity_result.txt", "r");
     if (fp_result == NULL) {
         printf("Failed to open file: similarity_result.txt\n");
@@ -7,7 +7,7 @@
 
     for (int i = 0; i < 2; i++) {
         printf("|");
-        for (int j = 0; j < 100; j++) {
+        for (int j = 0; j < 76; j++) {
             printf(" ");
         }
         printf("|\n");
@@ -17,8 +17,8 @@
         printf("| ");
         char buf[100];
         fgets(buf, sizeof(buf), fp_result);
-        int len = (100 - strlen(buf)) / 2;
-        for (int j = 0; j < 100 - len - strlen(buf) - 1; j++) {
+        int len = (76 - strlen(buf)) / 2;
+        for (int j = 0; j < 76 - len - strlen(buf) - 1; j++) {
             printf(" ");
         }
         printf("%s", buf);
@@ -30,10 +30,11 @@
 
     for (int i = 0; i < 2; i++) {
         printf("|");
-        for (int j = 0; j < 100; j++) {
+        for (int j = 0; j < 76; j++) {
             printf(" ");
         }
         printf("|\n");
     }
 
-    printf(" ---------------------------------------------------------------------------------------------------");
+    printf(" ---------------------------------------------------------------------------");
+
