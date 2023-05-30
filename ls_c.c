dc922cfe8398b8302441b23a5fdc8152f7f6b917
Widadada <106460325+Widadada@users.noreply.github.com>
2023-05-30 20:05:23 +0900

    Update ls_c.c
    +주석

--- ls_c.c
+++ ls_c.c
@@ -1,3 +1,4 @@
+// execute grep | ls
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
@@ -6,20 +7,25 @@
 #include <sys/wait.h>
 
 int main() {
-    char *grep = "grep";
-    char *ls = "ls";
-    char *c = "\\.c$";
+    char* grep = "grep";
+    char* ls = "ls";
+    char* c = "\\.c$";
 
     int pid;
     int thepipe[2];
+
+    // create a pipe
     if (pipe(thepipe) == -1) {
         perror("pipe");
         exit(1);
     }
+
+    // create a child process
     if ((pid = fork()) == -1) {
         perror("fork");
         exit(1);
     }
+    // create a parent process
     if (pid > 0) {
         close(thepipe[1]);
         dup2(thepipe[0], 0);
@@ -33,7 +39,7 @@ int main() {
     dup2(thepipe[1], 1);
     close(thepipe[1]);
     execlp(ls, ls, NULL);
-    
+
     perror("execlp");
     exit(1);
 }
