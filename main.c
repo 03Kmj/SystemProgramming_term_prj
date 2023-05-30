c096258f63e2efa25630e7f751d156d9b2c496b7
Widadada <106460325+Widadada@users.noreply.github.com>
2023-05-30 21:21:11 +0900

    Update main.c
    +주석

--- main.c
+++ main.c
@@ -9,57 +9,62 @@
 
 #define MAX_FILENAMES 100
 
-char *filenames[MAX_FILENAMES];
+char* filenames[MAX_FILENAMES];
 int num_files = 0;
 int file_cnt = 0;
 int main_check = 1;
-int num_sub_files= 0;
+int num_sub_files = 0;
 
 // Drag and Drop
-int drag_and_drop(int, char *[]);
+int drag_and_drop(int, char* []);
 void save_filenames_to_file();
-void start_button_clicked(GtkWidget *, gpointer);
-void drag_data_received(GtkWidget *, GdkDragContext *, int, int, GtkSelectionData *, guint, guint, gpointer);
+void start_button_clicked(GtkWidget*, gpointer);
+void drag_data_received(GtkWidget*, GdkDragContext*, int, int, GtkSelectionData*, guint, guint, gpointer);
 
-// print progress bar
+// Print progress bar
 void progress_bar();
 
-// call ls_c and check if there is enough C files
+// Call ls_c and Check if there is enough C files
 void ls_grep_c();
 
-// show intro
+// Show intro
 void intro();
 
-// print similarity with colors
+// Print similarity with colors
 void print_similarity();
 
-// signal handler
+// Signal handler
 void sigint_handler(int signum);
 void sigquit_handler(int signum);
 
-// main
-int main(int argc, char *argv[]) {
+// main function
+int main(int argc, char* argv[]) {
+
+    // Register SIGINT, SIGQUIT signal handler
     signal(SIGINT, sigint_handler);
     signal(SIGQUIT, sigquit_handler);
 
     char main_dir[100];
     char sub_dir[100];
-    
+
+    // Call the intro function
     intro();
 
     printf("\n======= Here is a list of files you can use =======\n");
+    // Call the ls_grep_c function
     ls_grep_c();
 
     printf("\n========= Here is a list of dropped files =========\n");
+    // Call the drap_and_drop function
     drag_and_drop(argc, argv);
 
-    FILE *fp = fopen("dropped_file.txt", "r");
+    FILE* fp = fopen("dropped_file.txt", "r");
     if (fp == NULL) {
         printf("Failed to open file: dropped_file.txt\n");
         exit(1);
     }
 
-    FILE *fp_result = fopen("similarity_result.txt", "w");
+    FILE* fp_result = fopen("similarity_result.txt", "w");
     if (fp_result == NULL) {
         printf("Failed to open file: similarity_result.txt\n");
         exit(1);
@@ -67,30 +72,37 @@ int main(int argc, char *argv[]) {
 
     main_check = 1;
     for (int i = 0; i < num_files; i++) {
+        // Buffer to store the file name
         char buf[100];
         fscanf(fp, "%s", buf);
 
+        // First file is the main file
         if (main_check) {
             strcpy(main_dir, buf);
             main_check = 0;
         }
+        // Other file is the sub files
         else {
             strcpy(sub_dir, buf);
 
+            // Buffer to store the command
             char command[512];
             sprintf(command, "python3 ./similarity.py %s %s", main_dir, sub_dir);
 
-            FILE *fp = popen(command, "r");
+            FILE* fp = popen(command, "r");
             if (fp == NULL) {
                 printf("Error: cannot open file\n");
                 return 1;
             }
 
+            // Variable to store the similarity value
             double similarity;
             fscanf(fp, "%lf", &similarity);
             pclose(fp);
 
             printf("\n");
+
+            // Call the progress_bar function
             progress_bar(sub_dir);
             fprintf(fp_result, "The similarity between %s and %s is %.2lf%%\n", main_dir, sub_dir, similarity);
             sleep(1);
@@ -100,14 +112,17 @@ int main(int argc, char *argv[]) {
     fclose(fp);
     fclose(fp_result);
 
+    // Call the print_similarity function
     print_similarity();
 
     return 0;
 }
 
 void sigint_handler(int signum) {
+    // Set the text color to yellow
     printf("\033[33m");
     printf("\nCtrl-C ignored\n");
+    // Reset the text color to the default
     printf("\033[0m");
 }
 
@@ -117,38 +132,52 @@ void sigquit_handler(int signum) {
     printf("\033[0m");
 }
 
-int drag_and_drop(int argc, char *argv[]) {
-    GtkWidget *window;
-    GtkWidget *main_box;
-    GtkWidget *drop_label;
-    GtkWidget *start_button;
+int drag_and_drop(int argc, char* argv[]) {
+    // Window widget
+    GtkWidget* window;
+    // Main box widget
+    GtkWidget* main_box;
+    // Drop label widget
+    GtkWidget* drop_label;
+    // Start button widget
+    GtkWidget* start_button;
+    // Specify the supported drag and drop target type
     GtkTargetEntry targets[] = {
         { "text/uri-list", 0, 0 }
     };
 
+    // Initialize GTK
     gtk_init(&argc, &argv);
 
+    // Create a new top-level window
     window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
     gtk_widget_set_size_request(window, 400, 300);
+    // Connect the destroy signal to the gtk_main_quit function
     g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
 
+    // Create a new vertical box
     main_box = gtk_box_new(GTK_ORIENTATION_VERTICAL, 10);
     gtk_container_add(GTK_CONTAINER(window), main_box);
 
+    // Create a new label widget
     drop_label = gtk_label_new("Drop C File here!");
     gtk_box_pack_start(GTK_BOX(main_box), drop_label, TRUE, TRUE, 0);
 
+    // Create a new button widget
     start_button = gtk_button_new_with_label("Start Similarity Check!");
     g_signal_connect(start_button, "clicked", G_CALLBACK(start_button_clicked), NULL);
     gtk_box_pack_start(GTK_BOX(main_box), start_button, TRUE, TRUE, 0);
 
+    // Set drop_label as a drag destination with specified targets and actions
     gtk_drag_dest_set(drop_label, GTK_DEST_DEFAULT_ALL, targets, G_N_ELEMENTS(targets), GDK_ACTION_COPY);
     g_signal_connect(drop_label, "drag-data-received", G_CALLBACK(drag_data_received), NULL);
 
     gtk_widget_show_all(window);
 
+    // Start the GTK main loop
     gtk_main();
 
+    // Call the save_filenames_to_file function
     save_filenames_to_file();
 
     for (int i = 0; i < num_files; i++) {
@@ -157,7 +186,7 @@ int drag_and_drop(int argc, char *argv[]) {
 }
 
 void save_filenames_to_file() {
-    FILE *fp = fopen("dropped_file.txt", "w");
+    FILE* fp = fopen("dropped_file.txt", "w");
     if (fp == NULL) {
         printf("Failed to open file for writing.\n");
         exit(1);
@@ -168,32 +197,36 @@ void save_filenames_to_file() {
     fclose(fp);
 }
 
-void start_button_clicked(GtkWidget *widget, gpointer user_data) {
+void start_button_clicked(GtkWidget* widget, gpointer user_data) {
+    // Quit the GTK main loop
     gtk_main_quit();
 }
 
-void drag_data_received(GtkWidget *widget, GdkDragContext *context,
-                        int x, int y, GtkSelectionData *data,
-                        guint info, guint time, gpointer user_data)
+void drag_data_received(GtkWidget* widget, GdkDragContext* context,
+    int x, int y, GtkSelectionData* data,
+    guint info, guint time, gpointer user_data)
 {
-    char **uris = gtk_selection_data_get_uris(data);
+    char** uris = gtk_selection_data_get_uris(data);
     if (uris) {
-        char **uri;
+        char** uri;
         for (uri = uris; *uri; uri++) {
-            GFile *file = g_file_new_for_uri(*uri);
-            char *filename = g_file_get_basename(file);
+            GFile* file = g_file_new_for_uri(*uri);
+            char* filename = g_file_get_basename(file);
             g_object_unref(file);
 
-            char *extension = strrchr(filename, '.');
+            // Find the last occurrence of '.' in the filename
+            char* extension = strrchr(filename, '.');
             if (extension == NULL || strcmp(extension, ".c") != 0) {
+                // Set the text color to red
                 printf("\033[1;31m");
                 printf("Unsupported Extension: ");
+                // Reset the text color to the default
                 printf("\033[0m");
                 printf("%s is not a C file.\n", filename);
                 continue;
             }
 
-            FILE *fp = fopen(filename, "r");
+            FILE* fp = fopen(filename, "r");
             if (fp == NULL) {
                 printf("\033[1;31m");
                 printf("Invalid Directory: ");
@@ -205,6 +238,7 @@ void drag_data_received(GtkWidget *widget, GdkDragContext *context,
 
             filenames[num_files] = g_strdup(filename);
 
+            // Print the filename as the main file & sub file
             if (main_check) {
                 g_print("Main file: %s\n", filename);
                 main_check = 0;
@@ -216,27 +250,31 @@ void drag_data_received(GtkWidget *widget, GdkDragContext *context,
         }
         g_strfreev(uris);
     }
+    // Finish the drag operation
     gtk_drag_finish(context, TRUE, FALSE, time);
 }
 
-void progress_bar(char *filename) {
+void progress_bar(char* filename) {
     const char bar = '#';
     const char blank = ' ';
+    // Length of the progress bar
     const int LEN = 50;
     const int MAX = 100;
     const int SPEED = 10;
+    // Current progress count
     int count = 0;
     int i;
-    float tick = (float) MAX / LEN;
+    // Value indicating the progress increment for each bar segment
+    float tick = (float)MAX / LEN;
     int barCount;
     float percent;
-    
+
     while (count <= MAX) {
         printf("\r(%d/%d) %s ", file_cnt, num_sub_files, filename);
         printf("\033[32m");
         printf("[");
-        percent = (float) count / MAX * 100;
-        barCount = (int) (percent / tick);
+        percent = (float)count / MAX * 100;
+        barCount = (int)(percent / tick);
         for (i = 0; i < LEN; i++) {
             if (i < barCount)
                 printf("%c", bar);
@@ -258,7 +296,8 @@ void progress_bar(char *filename) {
 }
 
 void ls_grep_c() {
-    FILE *pipe = popen("./ls_c", "r");
+    // Execute the 'ls_c' command and Read the output through a pipe
+    FILE* pipe = popen("./ls_c", "r");
     if (pipe == NULL) {
         perror("popen");
         exit(1);
@@ -266,6 +305,8 @@ void ls_grep_c() {
 
     char buffer[1024];
     int c_check = 0;
+    
+    // Read each line from the pipe
     while (fgets(buffer, sizeof(buffer), pipe) != NULL) {
         printf("%s", buffer);
         c_check++;
@@ -279,6 +320,7 @@ void ls_grep_c() {
     }
 }
 
+// Print intro message
 void intro() {
     printf("\n\n");
     printf("*Our Program: Similarity check\n");
@@ -287,21 +329,22 @@ void intro() {
     sleep(2);
     printf("\n\n\n");
 
-    printf("\033[1;31m     _           _ _            _ _                _               _      \n");  
-    printf("\033[1;31m    (_)         (_) |          (_) |              | |             | |     \n");  
-    printf("\033[1;33m ___ _ _ __ ___  _| | __ _ _ __ _| |_ _   _    ___| |__   ___  ___| | __  \n");  
-    printf("\033[1;32m/ __| | '_ ` _ \\| | |/ _` | '__| | __| | | |  / __| '_ \\ / _ \\/ __| |/ /  \n");  
-    printf("\033[1;36m\\__ \\ | | | | | | | | (_| | |  | | |_| |_| | | (__| | | |  __/ (__|   <   \n");  
-    printf("\033[1;34m|___/_|_| |_| |_|_|_|\\__,_|_|  |_|\\__|\\__, |  \\___|_| |_|\\___|\\___|_|\\_\\  \n");  
-    printf("\033[1;35m                                       __/ |                               \n");  
-    printf("\033[1;35m                                      |___/                                \n");  
-    printf("\033[0m"); 
+    printf("\033[1;31m     _           _ _            _ _                _               _      \n");
+    printf("\033[1;31m    (_)         (_) |          (_) |              | |             | |     \n");
+    printf("\033[1;33m ___ _ _ __ ___  _| | __ _ _ __ _| |_ _   _    ___| |__   ___  ___| | __  \n");
+    printf("\033[1;32m/ __| | '_ ` _ \\| | |/ _` | '__| | __| | | |  / __| '_ \\ / _ \\/ __| |/ /  \n");
+    printf("\033[1;36m\\__ \\ | | | | | | | | (_| | |  | | |_| |_| | | (__| | | |  __/ (__|   <   \n");
+    printf("\033[1;34m|___/_|_| |_| |_|_|_|\\__,_|_|  |_|\\__|\\__, |  \\___|_| |_|\\___|\\___|_|\\_\\  \n");
+    printf("\033[1;35m                                       __/ |                               \n");
+    printf("\033[1;35m                                      |___/                                \n");
+    printf("\033[0m");
     sleep(2);
     printf("\n\n\n");
 }
 
+// Print similarity result
 void print_similarity() {
-    FILE *fp_result = fopen("similarity_result.txt", "r");
+    FILE* fp_result = fopen("similarity_result.txt", "r");
     if (fp_result == NULL) {
         printf("Failed to open file: similarity_result.txt\n");
         exit(1);
@@ -327,44 +370,49 @@ void print_similarity() {
         fgets(text, sizeof(text), fp_result);
 
         int len = (70 - strlen(text)) / 2;
-        for (int j = 0; j < 70 -len - strlen(text) - 1; j++) {
+        for (int j = 0; j < 70 - len - strlen(text) - 1; j++) {
             printf(" ");
         }
-        
-        const char *keyword = "is";
 
-        char *keyword_position = strstr(text, keyword);
+        const char* keyword = "is";
+
+        char* keyword_position = strstr(text, keyword);
 
         size_t before_keyword = keyword_position - text;
-        printf("%.*s", (int) before_keyword, text);
+        printf("%.*s", (int)before_keyword, text);
         printf("%s", keyword);
 
-        char *percentage_position = strstr(keyword_position, "%");
-        char *number_start = keyword_position + strlen(keyword) + 1;
+        char* percentage_position = strstr(keyword_position, "%");
+        char* number_start = keyword_position + strlen(keyword) + 1;
         size_t number_length = percentage_position - number_start;
         char number_string[50];
         strncpy(number_string, number_start, number_length);
         number_string[number_length] = '\0';
         double number = atof(number_string);
 
-        const char *color_code;
+        const char* color_code;
         if (number > 90) {
+            // Set the text color to red for high similarity
             color_code = "\033[31m";
         }
         else if (number > 70) {
+            // Set the text color to magenta for medium-high similarity
             color_code = "\033[35m";
         }
         else if (number > 50) {
+            // Set the text color to blue for medium similarity
             color_code = "\033[34m";
         }
         else if (number > 30) {
+            // Set the text color to yellow for medium-low similarity
             color_code = "\033[33m";
         }
         else {
+            // Set the text color to green for low similarity
             color_code = "\033[32m";
         }
 
-        int offset = (int) (percentage_position - keyword_position - strlen(keyword));
+        int offset = (int)(percentage_position - keyword_position - strlen(keyword));
 
         printf("%s", color_code);
         printf("%.*s", offset, keyword_position + strlen(keyword));
