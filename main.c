c211596b40fb15b5a454d580758990d7dfc7f0f5
seokhyunpark <122147233+seokhyunpark@users.noreply.github.com>
2023-05-31 21:59:03 +0900

    Update main.c
    프로그레스바 스레드로 구현
    기타 오류들 수정

--- main.c
+++ main.c
@@ -4,16 +4,26 @@
 #include <string.h>
 #include <fcntl.h>
 #include <sys/wait.h>
+#include <pthread.h>
+#include <signal.h>
+#include <dirent.h>
 #include <gtk/gtk.h>
 #include <gio/gio.h>
 
 #define MAX_FILENAMES 100
+
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+pthread_t threads[MAX_FILENAMES];
+char sub_file_list[MAX_FILENAMES][MAX_FILENAMES];
 
 char* filenames[MAX_FILENAMES];
 int num_files = 0;
 int file_cnt = 0;
 int main_check = 1;
 int num_sub_files = 0;
+int num_c_files = 1;
+int dropped_try = 1;
+int c_in_the_dir = 0;
 
 // Drag and Drop
 int drag_and_drop(int, char* []);
@@ -22,7 +32,7 @@ void start_button_clicked(GtkWidget*, gpointer);
 void drag_data_received(GtkWidget*, GdkDragContext*, int, int, GtkSelectionData*, guint, guint, gpointer);
 
 // Print progress bar
-void progress_bar();
+void *progress_bar();
 
 // Call ls_c and Check if there is enough C files
 void ls_grep_c();
@@ -47,6 +57,8 @@ int main(int argc, char* argv[]) {
     char main_dir[100];
     char sub_dir[100];
 
+    system("clear");
+
     // Call the intro function
     intro();
 
@@ -54,9 +66,35 @@ int main(int argc, char* argv[]) {
     // Call the ls_grep_c function
     ls_grep_c();
 
+    DIR *dir;
+    struct dirent *entry;
+
+    dir = opendir(".");
+    if (dir == NULL) {
+        perror("opendir");
+        return 1;
+    }
+
+    while ((entry = readdir(dir)) != NULL) {
+        if (entry->d_type == DT_REG && strstr(entry->d_name, ".c") != NULL)
+            c_in_the_dir++;
+    }
+
+    closedir(dir);
+
     printf("\n========= Here is a list of dropped files =========\n");
     // Call the drap_and_drop function
     drag_and_drop(argc, argv);
+
+    FILE *dropped_file = fopen("dropped_file.txt", "r");
+    if (dropped_file == NULL) {
+        printf("Failed to open file: dropped_file.txt\n");
+        exit(1);
+    }
+    for (int i = 0; i < num_files; i++) {
+        fscanf(dropped_file, "%s", sub_file_list[i]);
+        num_c_files++;
+    }
 
     FILE* fp = fopen("dropped_file.txt", "r");
     if (fp == NULL) {
@@ -102,17 +140,43 @@ int main(int argc, char* argv[]) {
 
             printf("\n");
 
-            // Call the progress_bar function
-            progress_bar(sub_dir);
             fprintf(fp_result, "The similarity between %s and %s is %.2lf%%\n", main_dir, sub_dir, similarity);
-            sleep(1);
         }
         file_cnt++;
     }
     fclose(fp);
     fclose(fp_result);
 
+    int t = 0;
+    for (t = 0; t < num_sub_files; t++) {
+        if (pthread_create(&threads[t], NULL, progress_bar, (void *)sub_file_list[t+1]) != 0) {
+            fprintf(stderr, "Error creating thread %d\n", t);
+            exit(1);
+        }
+        usleep(500000);
+    }
+
+
+    for (t = 0; t < num_sub_files; t++) {
+        if (pthread_join(threads[t], NULL) != 0) {
+            fprintf(stderr, "Error joining thread %d\n", t);
+            exit(1);
+        }
+    }
+
     // Call the print_similarity function
+    usleep(500000);
+    printf("\n\n\n");
+    printf("\033[1;31m"); printf("Re");
+    printf("\033[1;33m"); printf("su");
+    printf("\033[1;32m"); printf("lt");
+    printf("\033[1;36m"); printf("i");
+    printf("\033[1;34m"); printf("ng");
+    printf("\033[1;35m"); printf("...");
+    printf("\033[0m");
+
+    printf("\n");
+    sleep(3);
     print_similarity();
 
     return 0;
@@ -247,6 +311,7 @@ void drag_data_received(GtkWidget* widget, GdkDragContext* context,
                 g_print("Sub file (%d): %s\n", ++num_sub_files, filename);
             }
             num_files++;
+            dropped_try++;
         }
         g_strfreev(uris);
     }
@@ -254,7 +319,8 @@ void drag_data_received(GtkWidget* widget, GdkDragContext* context,
     gtk_drag_finish(context, TRUE, FALSE, time);
 }
 
-void progress_bar(char* filename) {
+void *progress_bar(char *arg) {
+    char *filename = (char *)arg;
     const char bar = '#';
     const char blank = ' ';
     // Length of the progress bar
@@ -268,9 +334,19 @@ void progress_bar(char* filename) {
     float tick = (float)MAX / LEN;
     int barCount;
     float percent;
+    int thread_id;
+
+    pthread_mutex_lock(&mutex);
+    thread_id = file_cnt;
+    file_cnt++;
+    pthread_mutex_unlock(&mutex);
 
     while (count <= MAX) {
-        printf("\r(%d/%d) %s ", file_cnt, num_sub_files, filename);
+        int line = 18 + dropped_try + num_c_files + c_in_the_dir + thread_id;
+
+        pthread_mutex_lock(&mutex);
+        printf("\033[%d;0H", line);
+        printf("%s ", filename);
         printf("\033[32m");
         printf("[");
         percent = (float)count / MAX * 100;
@@ -285,14 +361,17 @@ void progress_bar(char* filename) {
         printf("\033[0m");
         printf("%.2f%%", percent);
         fflush(stdout);
+        pthread_mutex_unlock(&mutex);
+
         count++;
         usleep(SPEED * 1000);
+
+        pthread_mutex_lock(&mutex);
+        printf("\033[K");
+        pthread_mutex_unlock(&mutex);
     }
-    printf("\r(%d/%d) %s [", file_cnt, num_sub_files, filename);
-    for (i = 0; i < LEN; i++) {
-        printf("%c", bar);
-    }
-    printf("] 100.00%%");
+
+    pthread_exit(NULL);
 }
 
 void ls_grep_c() {
@@ -322,12 +401,11 @@ void ls_grep_c() {
 
 // Print intro message
 void intro() {
-    printf("\n\n");
     printf("*Our Program: Similarity check\n");
     printf("*Purpose: This program checks the similarity between c codes.\n");
     printf("*Writer: mjkang, shpark, hjwi\n");
     sleep(2);
-    printf("\n\n\n");
+    printf("\n\n");
 
     printf("\033[1;31m     _           _ _            _ _                _               _      \n");
     printf("\033[1;31m    (_)         (_) |          (_) |              | |             | |     \n");
@@ -339,7 +417,7 @@ void intro() {
     printf("\033[1;35m                                      |___/                                \n");
     printf("\033[0m");
     sleep(2);
-    printf("\n\n\n");
+    printf("\n\n");
 }
 
 // Print similarity result
